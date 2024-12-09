ccefbd45af2c7e868a9851a5711d29291a13ad98
Slente468 <GL51HW@student.aau.dk>
2024-12-09 03:12:41 +0100

    UpdatingHorseMovmentScript
    to see if we can get the mount thing to work.

--- YouSayRunE24/Assets/Scripts/HorseMovement.cs
+++ YouSayRunE24/Assets/Scripts/HorseMovement.cs
@@ -4,49 +4,107 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class HorseMovement : MonoBehaviour
-{
-    //the animator on the horse. 
-    Animator horseAnimator;
-    // bools under the animator:
-    bool isHWalking = false;
-    bool isHRunning = false;
-    bool isHIdle = true;
-    bool isHJumping = false;
+{ // This one is basically a copy of PM just for horse. 
+    // Movement inputs is raw
+    private float x_input;
+    private float z_input;
+    //Reminder: AmericanZ, Horizontal/Vertical AD/WS, transform.forward = z
+
+    //makes space for the 3 values vector moveDirection; is currently empty.
+    Vector3 moveDirection;
+
+    // Reference to Rigidbody for movement
+    Rigidbody rb;
+    //Reminder: this be empty; it needs to be filled with the component in question.
+
+    //Variable, This is defualt walkingSpeed, for method and animation.
+    [SerializeField] private float walkingSpeed = 7f;
+    [SerializeField] private float jumpPower = 8f;
+    [SerializeField] private float runningSpeed = 15f;
+
+    // Animator reference
+    [SerializeField] private Animator HorseAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize Rigidbody and Animator components
+        rb = GetComponent<Rigidbody>();
+        //playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get player input and determine move direction
+        x_input = Input.GetAxisRaw("Horizontal");
+        z_input = Input.GetAxisRaw("Vertical");
+
+        // Normalize the movement direction
+        moveDirection = new Vector3(x_input, 0f, z_input).normalized;
+
+        // Handles rotation and animations of player
+        PlayerRotationAndAnimations();
+
+        //jump method
+        PlayerInputJump();
+
+        // Move the player
+        if (moveDirection != Vector3.zero)
+        {
+            rb.velocity = new Vector3(moveDirection.x * walkingSpeed, rb.velocity.y, moveDirection.z * walkingSpeed);
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                rb.velocity = new Vector3(moveDirection.x * runningSpeed, rb.velocity.y, moveDirection.z * runningSpeed);
+            }
+        }
     }
 
 
 
-    void DetectWASD_HMovementInput()
+
+    // Handle rotation and animations
+    private void PlayerRotationAndAnimations()
     {
-        // If detecting movement inputs from the WASD keys, 
-        bool isMoving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
-
-
-
-        if (isMoving == true)
+        if (moveDirection != Vector3.zero)
         {
-            isHIdle = false;
-            isHIdle = false;
+            playerAnimator.SetBool("isPIdle", false);
+            // Rotate to face the movement direction
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
 
-            isHWalking = !Input.GetKey(KeyCode.LeftShift);
-            isHRunning = Input.GetKey(KeyCode.LeftShift);
-
+            // Play walking animation
+            playerAnimator.SetBool("isPWalking", true);
         }
         else
         {
-            isHIdle = true;
-            isHWalking = false;
-            isHRunning = false;
+            // Play idle animation
+            playerAnimator.SetBool("isPWalking", false);
+            playerAnimator.SetBool("isPIdle", true);
+            //taking no chances
         }
     }
-        // Update is called once per frame
-            void Update()
-            {
-        
-            }
+    void PlayerInputJump()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //rb.AddForce.up = jump;
+            //rb.AddForce(new Vector3(rb.velocity.x, rb.velocity.y*jumpPower,rb.velocity.z));
+            rb.AddForce(new Vector3(rb.velocity.x, jumpPower, rb.velocity.z), ForceMode.Impulse);
+            Debug.Log("we jumped.");
+            playerAnimator.SetBool("isPJumping", true);
+
+
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            playerAnimator.SetBool("isPJumping", false);
+        }
+
+
+
+    }
 }
